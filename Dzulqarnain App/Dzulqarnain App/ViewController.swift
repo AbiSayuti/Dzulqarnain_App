83a5cc151cc538e8d31cb99b864c8c4ea8782a10
Abi SA <abisa3002@gmail.com>
2017-12-02 16:29:18 +0700

    Done

---
+++ Dzulqarnain App/Dzulqarnain App/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Dzulqarnain App
+//
+//  Created by Abi Sayuti on 12/2/17.
+//  Copyright © 2017 Abi Sayuti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var Dzul: UIWebView!
+    
+    let urlweb = "http://dzulqarnain.net/"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let requesturl = NSURL(string: urlweb)
+        
+        
+        let request = NSURLRequest(url: requesturl! as URL)
+        Dzul.loadRequest(request as URLRequest)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
